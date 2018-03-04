95d7caa0540f2192d7127e946b4b1166697bdbfd
Swojit Mohapatra <neelmohapatra@gmail.com>
2018-03-04 15:05:31 -0800

    Updated index.ts

--- packages/vega2-extension/src/index.ts
+++ packages/vega2-extension/src/index.ts
@@ -4,7 +4,11 @@
 |----------------------------------------------------------------------------*/
 
 import {
+<<<<<<< Updated upstream
   JSONObject, JSONValue, ReadonlyJSONObject
+=======
+  JSONObject
+>>>>>>> Stashed changes
 } from '@phosphor/coreutils';
 
 import {
@@ -19,8 +23,12 @@ import {
  * Import vega-embed in this manner due to how it is exported.
  */
 // Import only the typings for vega-embed-v2 - do not use for values.
+<<<<<<< Updated upstream
 import embed = require('vega-embed-v2');
 
+=======
+import vegaEmbed, {Mode} from 'vega-embed';
+>>>>>>> Stashed changes
 
 import '../style/index.css';
 
@@ -75,6 +83,7 @@ class RenderedVega2 extends Widget implements IRenderMime.IRenderer {
     let mimeType = this._mimeType = options.mimeType;
     if (mimeType === VEGA_MIME_TYPE) {
       this.addClass(VEGA_CLASS);
+<<<<<<< Updated upstream
       this._mode = 'vega';
     } else {
       this.addClass(VEGALITE_CLASS);
@@ -121,6 +130,65 @@ class RenderedVega2 extends Widget implements IRenderMime.IRenderer {
 
   private _mimeType: string;
   private _mode: string;
+=======
+    } else {
+      this.addClass(VEGALITE_CLASS);
+    }
+  }
+
+  // /**
+  //  * Render Vega/Vega-Lite into this widget's node.
+  //  */
+  // renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+
+  //   let data = model.data[this._mimeType] as ReadonlyJSONObject;
+  //   let updatedData: JSONObject;
+  //   if (this._mode === 'vega-lite') {
+  //     updatedData = Private.updateVegaLiteDefaults(data);
+  //   } else {
+  //     updatedData = data as JSONObject;
+  //   }
+
+  //   let embedSpec = {
+  //     mode: this._mode,
+  //     spec: updatedData
+  //   };
+
+  //   return Private.ensureMod().then(embedFunc => {
+  //     return new Promise<void>((resolve, reject) => {
+  //       embedFunc(this.node, embedSpec, (error: any, result: any) => {
+  //       //   if (error) {
+  //       //     return reject(error);
+  //       //   }
+
+  //       //   // Save png data in MIME bundle along with original MIME data.
+  //       //   if (!model.data['image/png']) {
+  //       //     let imageData = result.view.toImageURL().split(',')[1] as JSONValue;
+  //       //     let newData = {...(model.data), 'image/png': imageData};
+  //       //     model.setData({ data: newData });
+  //       //   }
+  //       //   resolve(undefined);
+  //       });
+  //     });
+  //   });
+  // }
+    /**
+   * Render Vega/Vega-Lite into this widget's node.
+   */
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    const data = model.data[this._mimeType] as JSONObject;
+    const mode: Mode = this._mimeType === VEGA_MIME_TYPE ? 'vega' : 'vega-lite';
+    const options = {
+      mode,
+      actions: true
+    };
+    return vegaEmbed(this.node as HTMLBaseElement, data, options).then((result) => {
+      console.log(result);
+    }).catch(console.warn);
+  }
+
+  private _mimeType: string;
+>>>>>>> Stashed changes
 }
 
 
@@ -170,6 +238,7 @@ const extension: IRenderMime.IExtension = {
 export default extension;
 
 
+<<<<<<< Updated upstream
 /**
  * Namespace for module privates.
  */
@@ -234,3 +303,69 @@ namespace Private {
     }
   }
 }
+=======
+// /**
+//  * Namespace for module privates.
+//  */
+// namespace Private {
+
+//   /**
+//    * Default cell config for Vega-Lite.
+//    */
+//   const defaultCellConfig: JSONObject = {
+//     'width': 400,
+//     'height': 400 / 1.5
+//   };
+
+//   /**
+//    * The embed module import.
+//    */
+//   let mod: typeof embed;
+
+//   /**
+//    * Initialize the vega-embed module.
+//    */
+//   export
+//   function ensureMod(): Promise<typeof embed> {
+//     return new Promise((resolve, reject) => {
+//       if (mod !== undefined) {
+//         resolve(mod);
+//         return;
+//       }
+//       (require as any).ensure(['vega-embed'], (require: NodeRequire) => {
+//         mod = require('vega-embed');
+//         resolve(mod);
+//       },
+//       (err: any) => {
+//         reject(err);
+//       },
+//       'vega2'
+//       );
+//     });
+//   }
+
+//   /**
+//    * Apply the default cell config to the spec in place.
+//    *
+//    * #### Notes
+//    * This carefully does a shallow copy to avoid copying the potentially
+//    * large data.
+//    */
+//   export
+//   function updateVegaLiteDefaults(spec: ReadonlyJSONObject): JSONObject {
+//     let config = spec.config as JSONObject;
+//     if (!config) {
+//       return {...{'config': {'cell': defaultCellConfig}}, ...spec};
+//     }
+//     let cell = config.cell as JSONObject;
+//     if (cell) {
+//       return {
+//         ...{'config': {...{'cell': {...defaultCellConfig, ...cell}}}, ...config},
+//         ...spec
+//       };
+//     } else {
+//       return {...{'config': {...{'cell': {...defaultCellConfig}}}, ...config}, ...spec};
+//     }
+//   }
+// }
+>>>>>>> Stashed changes
